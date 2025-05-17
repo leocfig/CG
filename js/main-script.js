e11eb193e9a2161a126f70fd3d84c3a0c5ace251
Leonor Figueira <leonor.figueira@tecnico.ulisboa.pt>
2025-05-17 12:49:54 +0100

    feat: implement antennas + torso + abdomen + waist

--- js/main-script.js
+++ js/main-script.js
@@ -8,22 +8,48 @@ import { GUI } from "three/addons/libs/lil-gui.module.min.js";
 /* CONSTANTS */
 ///////////////
 
-// Head
+// Torso
+const TORSO_WIDTH = 30;
+const TORSO_HEIGHT = 16;
+const TORSO_DEPTH = 15;
+const TORSO_OFFSET_X = 0;
+const TORSO_OFFSET_Y = 0;
+const TORSO_OFFSET_Z = 0;
+
+// Abdomen (relative to torso center)
+const ABDOMEN_WIDTH  = 14;
+const ABDOMEN_HEIGHT = 4;
+const ABDOMEN_DEPTH  = 8;
+const ABDOMEN_OFFSET_X = 0;
+const ABDOMEN_OFFSET_Y = -TORSO_HEIGHT / 2 - ABDOMEN_HEIGHT / 2;
+const ABDOMEN_OFFSET_Z = 0;
+
+// Waist (relative to torso center)
+const WAIST_WIDTH  = 20;
+const WAIST_HEIGHT = 8;
+const WAIST_DEPTH  = 10;
+const WAIST_OFFSET_X = 0;
+const WAIST_OFFSET_Y = ABDOMEN_OFFSET_Y - ABDOMEN_HEIGHT / 2 - WAIST_HEIGHT / 2;
+const WAIST_OFFSET_Z = 0;
+
+// Head (relative to torso center)
 const HEAD_SIZE = 10;
 const HEAD_OFFSET_X = 0;
-const HEAD_OFFSET_Y = 8;
+const HEAD_OFFSET_Y = TORSO_HEIGHT / 2 + HEAD_SIZE / 2;
 const HEAD_OFFSET_Z = 0;
 
 // Eyes (relative to head center)
-const EYE_SIZE = 0.8;
-const EYE_OFFSET_X = HEAD_SIZE * 0.25;
-const EYE_OFFSET_Y = HEAD_SIZE * 0.95;
-const EYE_OFFSET_Z = HEAD_SIZE * 0.55;
+const EYE_SIZE = 0.9;
+const EYE_OFFSET_X = HEAD_SIZE / 5;
+const EYE_OFFSET_Y = HEAD_SIZE / 8;
+const EYE_OFFSET_Z = HEAD_SIZE / 2 + EYE_SIZE / 2;
 
-// Torso
-const TORSO_WIDTH = 10;
-const TORSO_HEIGHT = 12;
-const TORSO_DEPTH = 5;
+// Antennas (relative to head center)
+const ANTENNA_RADIUS = 0.7;
+const ANTENNA_HEIGHT = 9;
+const ANTENNA_OFFSET_X = HEAD_SIZE / 2 + ANTENNA_RADIUS;
+const ANTENNA_OFFSET_Y = HEAD_SIZE / 2.5;
+const ANTENNA_OFFSET_Z = 0;
 
 // Arms
 const ARM_WIDTH = 2;
@@ -42,6 +68,14 @@ const FOOT_HEIGHT = 1;
 const FOOT_DEPTH = 3;
 const FOOT_OFFSET_Y = LEG_OFFSET_Y - LEG_HEIGHT / 2 - FOOT_HEIGHT / 2;
 
+const materials = {
+    torso:    new THREE.MeshBasicMaterial({ color: 0x8b0000, wireframe: false }), // dark red
+    abdomen:  new THREE.MeshBasicMaterial({ color: 0x26428B, wireframe: false }), // dark navy blue
+    waist:    new THREE.MeshBasicMaterial({ color: 0x8b0000, wireframe: false }), // dark red
+    head:     new THREE.MeshBasicMaterial({ color: 0x26428B, wireframe: false }), // dark navy blue
+    eyes:     new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: false }), // black
+    antennas: new THREE.MeshBasicMaterial({ color: 0x26428B, wireframe: false })  // dark navy blue
+};
 
 //////////////////////
 /* GLOBAL VARIABLES */
@@ -49,7 +83,7 @@ const FOOT_OFFSET_Y = LEG_OFFSET_Y - LEG_HEIGHT / 2 - FOOT_HEIGHT / 2;
 
 let camera, frontCamera, sideCamera, topCamera, perspectiveCamera;
 let scene, renderer;
-let head;
+let headGroup, waistGroup, torso;
 let robot;
 
 /////////////////////
@@ -58,7 +92,6 @@ let robot;
 function createScene() {
     scene = new THREE.Scene();
     scene.background = new THREE.Color('#e8fcff');
-    //scene.add(new THREE.AxesHelper(10)); //?
     createRobot(0, 0, 0);
 }
 
@@ -100,6 +133,50 @@ function createCamera() {
 /* CREATE OBJECT3D(S) */
 ////////////////////////
 
+function addTorso(obj, x, y, z, material) {
+    torso = new THREE.Object3D();
+
+    const geometry = new THREE.BoxGeometry(TORSO_WIDTH, TORSO_HEIGHT, TORSO_DEPTH);
+    const torsoMesh = new THREE.Mesh(geometry, material);
+
+    torso.add(torsoMesh);
+    obj.add(torso);
+}
+
+function addAbdomen(obj, x, y, z, material) {
+    const geometry = new THREE.BoxGeometry(ABDOMEN_WIDTH, ABDOMEN_HEIGHT, ABDOMEN_DEPTH);
+    const abdomenMesh = new THREE.Mesh(geometry, material);
+    abdomenMesh.position.set(x, y, z);
+    obj.add(abdomenMesh);
+}
+
+function addWaist(obj, x, y, z, material) {
+    waistGroup = new THREE.Group();
+
+    const geometry = new THREE.BoxGeometry(WAIST_WIDTH, WAIST_HEIGHT, WAIST_DEPTH);
+    const waistMesh = new THREE.Mesh(geometry, material);
+    waistGroup.position.set(x, y, z);
+    waistGroup.add(waistMesh);
+
+    // TODO
+
+    obj.add(waistGroup);
+}
+
+function addHead(obj, x, y, z, material) {
+    headGroup = new THREE.Group();
+
+    const geometry = new THREE.BoxGeometry(HEAD_SIZE, HEAD_SIZE, HEAD_SIZE);
+    const headMesh = new THREE.Mesh(geometry, material);
+    headGroup.position.set(x, y, z);
+    headGroup.add(headMesh);
+
+    addEyes(headGroup, EYE_OFFSET_X, EYE_OFFSET_Y, EYE_OFFSET_Z, materials.eyes);
+    addAntennas(headGroup, ANTENNA_OFFSET_X, ANTENNA_OFFSET_Y, ANTENNA_OFFSET_Z, materials.antennas)
+
+    obj.add(headGroup);
+}
+
 function addEyes(obj, x, y, z, material) {
     const geometry = new THREE.BoxGeometry(EYE_SIZE, EYE_SIZE, EYE_SIZE);
 
@@ -112,25 +189,25 @@ function addEyes(obj, x, y, z, material) {
     obj.add(rightEye);
 }
 
-function addHead(obj, x, y, z, material) {
-    const headGroup = new THREE.Group();
+function addAntennas(obj, x, y, z, material) {
+    const geometry = new THREE.CylinderGeometry(ANTENNA_RADIUS, ANTENNA_RADIUS, ANTENNA_HEIGHT);
 
-    const geometry = new THREE.BoxGeometry(HEAD_SIZE, HEAD_SIZE, HEAD_SIZE);
-    const head = new THREE.Mesh(geometry, material);
-    head.position.set(x, y, z);
-    headGroup.add(head);
+    const leftAntenna = new THREE.Mesh(geometry, material);
+    leftAntenna.position.set(x, y, z);
+    obj.add(leftAntenna);
 
-    addEyes(headGroup, EYE_OFFSET_X, EYE_OFFSET_Y, EYE_OFFSET_Z, material);
-
-    obj.add(headGroup);
+    const rightAntenna = new THREE.Mesh(geometry, material);
+    rightAntenna.position.set(-x, y, z);
+    obj.add(rightAntenna);
 }
 
 function createRobot(x, y, z) {
     robot = new THREE.Object3D();
 
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-
-    addHead(robot, HEAD_OFFSET_X, HEAD_OFFSET_Y, HEAD_OFFSET_Z, material);
+    addTorso(robot,TORSO_OFFSET_X, TORSO_OFFSET_Y, TORSO_OFFSET_Z, materials.torso);
+    addAbdomen(robot, ABDOMEN_OFFSET_X, ABDOMEN_OFFSET_Y, ABDOMEN_OFFSET_Z,  materials.abdomen);
+    addWaist(robot, WAIST_OFFSET_X, WAIST_OFFSET_Y, WAIST_OFFSET_Z,  materials.waist);
+    addHead(torso, HEAD_OFFSET_X, HEAD_OFFSET_Y, HEAD_OFFSET_Z,  materials.head);
 
     scene.add(robot);
 
@@ -159,7 +236,7 @@ function render() {
     renderer.render(scene, camera);
 }
 
-////////////////////////////////
+//////////////head//////////////////
 /* INITIALIZE ANIMATION CYCLE */
 ////////////////////////////////
 function init() {
